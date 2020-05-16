79ff204ff724158795ca4e6b6c7a32eeada38762
Jem Gunay <jemgunay@Jems-MacBook-Pro.local>
2020-05-16 01:41:52 +0100

    Added more dialog.

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
+
+	"github.com/jemgunay/echo-pingpong/game"
 
 	alexa "github.com/mikeflynn/go-alexa/skillserver"
 )
@@ -41,37 +45,94 @@ func start(port int, skillID string) {
 func echoIntentHandler(w http.ResponseWriter, r *http.Request) {
 	echoReq := alexa.GetEchoRequest(r)
 
-	//g := game.Get(echoReq.GetSessionID())
-	log.Printf("session key: %s", echoReq.GetSessionID())
+	sessionKey := echoReq.GetSessionID()
+	g := game.Get(sessionKey)
+	log.Printf("session key: %s", sessionKey)
 
 	if echoReq.GetRequestType() == "LaunchRequest" {
 
 	} else if echoReq.GetRequestType() == "IntentRequest" {
 		switch echoReq.GetIntentName() {
+		case "AddPlayer":
+			name, err := validateName(echoReq)
+			if err != nil {
+				respond(w, err.Error())
+				return
+			}
+
+			if err := g.AddPlayer(name); err != nil {
+				respond(w, err.Error())
+				return
+			}
+
+			respond(w, name+" added")
+
 		case "StartGame":
 
-		case "ScorePoint":
+		case "PlayerScored":
+			name, err := validateName(echoReq)
+			if err != nil {
+				respond(w, err.Error())
+				return
+			}
+
+			if err := g.IncrementScore(name); err != nil {
+				respond(w, err.Error())
+				return
+			}
+
+			nextName, err := g.NextServe()
+			if err != nil {
+				respond(w, err.Error())
+				return
+			}
+
+			respond(w, nextName+" to serve")
 
 		case "QuitGame":
+			endSession(w, sessionKey)
 
 		default:
-
+			respond(w, "please can you repeat that again?")
 		}
 
 	} else if echoReq.GetRequestType() == "SessionEndedRequest" {
-
+		endSession(w, sessionKey)
 	}
-
-	startGame(w, "wanna play some ping pong fam")
 }
 
-func startGame(w http.ResponseWriter, message string) {
-	echoResp := alexa.NewEchoResponse().OutputSpeech(message).EndSession(true)
+func validateName(echoReq *alexa.EchoRequest) (string, error) {
+	slot, err := echoReq.GetSlot("Nickname")
+	if err != nil {
+		return "", errors.New("please provide a player name")
+	}
+
+	parts := strings.Split(slot.Value, " ")
+	if len(parts) > 3 {
+		return "", errors.New("player name is too long")
+	}
+
+	return slot.Value, nil
+}
+
+func respond(w http.ResponseWriter, message string) {
+	echoResp := alexa.NewEchoResponse().OutputSpeech(message)
 	json, err := echoResp.String()
 	if err != nil {
-		// TODO: handle error properly
 		return
 	}
 	w.Header().Set("Content-Type", "application/json;charset=UTF-8")
 	w.Write(json)
+}
+
+func endSession(w http.ResponseWriter, sessionKey string) {
+	echoResp := alexa.NewEchoResponse().EndSession(true)
+	json, err := echoResp.String()
+	if err != nil {
+		return
+	}
+	w.Header().Set("Content-Type", "application/json;charset=UTF-8")
+	w.Write(json)
+
+	game.Remove(sessionKey)
 }
